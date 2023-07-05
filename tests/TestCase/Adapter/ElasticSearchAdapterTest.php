584fb6dd15403bbdfd72b47c44289775c0babe57
dante di domenico <dante@didoda.com>
2023-07-05 12:19:36 +0200

    feat: add test

--- tests/TestCase/Adapter/ElasticSearchAdapterTest.php
+++ tests/TestCase/Adapter/ElasticSearchAdapterTest.php
@@ -122,6 +122,34 @@ class ElasticSearchAdapterTest extends TestCase
         $query = $this->fetchTable('objects')->find()->where(['id' => 1]);
         $text = 'searchme';
         $actual = $adapter->search($query, $text, []);
+        $partialSql = 'SELECT objects.id AS objects__id, objects.object_type_id AS objects__object_type_id, objects.status AS objects__status, objects.uname AS objects__uname, objects.locked AS objects__locked, objects.created AS objects__created, objects.modified AS objects__modified, objects.published AS objects__published, objects.title AS objects__title, objects.description AS objects__description, objects.body AS objects__body, objects.extra AS objects__extra, objects.lang AS objects__lang, objects.created_by AS objects__created_by, objects.modified_by AS objects__modified_by, objects.publish_start AS objects__publish_start, objects.publish_end AS objects__publish_end, objects.deleted AS objects__deleted, objects.custom_props AS objects__custom_props FROM objects objects INNER JOIN elasticsearch_';
+        static::assertInstanceOf(Query::class, $actual);
+        static::assertTrue(strpos($actual->sql(), $partialSql) === 0);
+    }
+
+    /**
+     * Test `search` with elastic search
+     *
+     * @return void
+     * @covers ::search()
+     * @covers ::buildQuery()
+     * @covers ::buildElasticSearchQuery()
+     * @covers ::createTempTable()
+     */
+    public function testSearchElastic(): void
+    {
+        $adapter = new class extends ElasticSearchAdapter {
+            protected function buildElasticSearchQuery(string $text, array $options): array
+            {
+                return [
+                    ['id' => 1, 'score' => 1.0],
+                    ['id' => 2, 'score' => 0.5],
+                ];
+            }
+        };
+        $query = $this->fetchTable('objects')->find()->where(['id' => 1]);
+        $text = 'searchme';
+        $actual = $adapter->search($query, $text, []);
         static::assertInstanceOf(Query::class, $actual);
         static::markTestIncomplete('This test has not been implemented yet.');
     }
